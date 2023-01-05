9ddd6af5d747505e17e905dea4165b76c6179b1c
kane04036 <hi011004@naver.com>
2023-01-06 04:15:28 +0900

    create sign up scene in view controller
    스토리보드에서 각 씬 만들고 이메일 뷰컨트롤러에 변수 연결

---
+++ ToDo/EmailForSignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EmailForSignUpViewController.swift
+//  ToDo
+//
+//  Created by 제임스 on 2023/01/06.
+//
+
+import UIKit
+
+class EmailForSignUpViewController : UIViewController{
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
