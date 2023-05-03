fcaf528644e5d1c9f12e3496cc7cca276c9633ad
Dasol <solda@Dasols-Laptop.local>
2023-05-03 14:22:04 +0900

    finish design

---
+++ TODORI/123.swift
@@ -0,0 +1,8 @@
+//
+//  123.swift
+//  TODORI
+//
+//  Created by Dasol on 2023/05/02.
+//
+
+import Foundation
--- TODORI/ViewController/APIConstant.swift
+++ TODORI/ViewController/APIConstant.swift
@@ -11,15 +11,10 @@ enum APIConstant {
     static let baseURL = "https://plotustodo-ctzhc.run.goorm.io"
     
     enum Account {
-        static let edit1 = baseURL + "/account/edit1/"
-        static let edit2 = baseURL + "/account/edit2/"
         static let emailCode = baseURL + "/account/emailcode/"
         static let login = baseURL + "/account/login/"
         static let logout = baseURL + "/account/logout/"
         static let register = baseURL + "/account/register/"
-        static let withdrawal = baseURL + "/account/withdrawal/"
-        static let who = baseURL + "/account/who/"
     }
     
-
 }
