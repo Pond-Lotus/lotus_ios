60673dcf3b263450bd85bd67abc21e3e761d94db
kane04036 <hi011004@naver.com>
2024-04-09 22:46:51 +0900

    change friend table view cell

--- TODORI/Source/ViewController/Friend/FriendTableViewCell.swift
+++ TODORI/Source/ViewController/Friend/FriendTableViewCell.swift
@@ -20,7 +20,6 @@ class FriendTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
